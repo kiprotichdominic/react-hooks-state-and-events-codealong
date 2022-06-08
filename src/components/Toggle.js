7a979cf6f324d57b8a66df76ac7d177394b9944c
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-06-08 13:24:42 +0300

    finish assignment about state and events

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn] = useState(false)
+
+  const color = isOn ? "red" : "green";
+  function setOn() {
+    setIsOn((isOn) => !isOn)
+  }
+
+  return <button style={{ background: color }} onClick={setOn}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
